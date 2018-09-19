baaadf2f07609537b08ae536c5ed39589955bb79
Marko Saarainen <marko.s.saarainen@gmail.com>
2018-09-19 09:13:22 +0300

    no comment

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # programming-basic
+#added folder condition-task
---
+++ condition-tasks/task 1/task 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ojelma selvittää onko annettu luku " +
+                "positivinen " +
+                "negatiivinen vai nolla ");
+            Console.WriteLine("Syötä luku");
+            string UserInput;
+
+
+        }
+    }
+}
