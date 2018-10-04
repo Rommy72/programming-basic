bdc0d5837936c4373c2f05893120e868459ec52f
Marko Saarainen <marko.s.saarainen@gmail.com>
2018-10-04 09:38:04 +0300

    initialized looptask3-1 try chance folder looptask 4.1 to looptask4-1

---
+++ looptasks/loopTask 3-1/loopTask 3-1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace loopTask_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee annetun luvun kertoman");
+            int number = 0;
+            int i = 1;
+            int fact = 1;
+            do
+            {
+                Console.Write("syötä luku: ");
+                number = int.Parse(Console.ReadLine());
+
+                if (number <= 0)
+                    Console.WriteLine("Virheellinen syöte!");
+
+
+
+            }while (number <= 0);
+
+            while (i <= number)
+            {
+                fact = fact * i;
+                i = i + 1;
+            }
+            Console.WriteLine($"Luvun{number}! = {fact}");
+
+        }
+    }
+}
