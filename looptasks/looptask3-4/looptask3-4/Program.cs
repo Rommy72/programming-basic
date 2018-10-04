075ae772529588478262da792eab6e61549808d1
Marko Saarainen <marko.s.saarainen@gmail.com>
2018-10-04 10:13:34 +0300

    initialized looptask 3-4

---
+++ looptasks/looptask3-4/looptask3-4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace looptask3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
