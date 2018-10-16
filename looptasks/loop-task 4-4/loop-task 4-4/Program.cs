cf13ddfd8ad3aa3668fb1548f378e47a2eb537d8
Marko Saarainen <marko.s.saarainen@gmail.com>
2018-10-16 14:46:28 +0300

    loop-task 4-4 initialized and done

---
+++ looptasks/loop-task 4-4/loop-task 4-4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace loop_task_4_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("simuloi kolikon heittoa");
+            Console.Write("kuinka monta kertaa kolikkoa heitetään");
+            int counter = int.Parse(Console.ReadLine());
+            int heads = 0;
+            int tails = 0;
+            Random rnd = new Random();
+
+            for (int i = 0; i < counter; i++)
+            {
+                int rndNumber = rnd.Next(2);
+                if (rndNumber == 0)
+                    tails++;
+                else
+                    heads++;
+
+            }
+            Console.WriteLine($"rahaa on heitetty{counter} kertaa.");
+            Console.WriteLine($"klaavoja tuli{tails} ja kruunuja {heads}.");
+            {
+
+            }
+        }
+    }
+}
