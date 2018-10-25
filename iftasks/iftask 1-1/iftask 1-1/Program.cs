8f91ba23d60c1ed6b8045cb3f581e7465fdab7fc
Marko Saarainen <marko.s.saarainen@gmail.com>
2018-10-25 13:31:48 +0300

    intitialized iftask folder and iftask 1-1 program and done

---
+++ iftasks/iftask 1-1/iftask 1-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace iftask_1_1
+{ 
+     class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ohjelma selvittää onko annettu luku " +
+            "positivinen " +
+            "negatiivinen vai nolla ");
+        Console.WriteLine("Syötä luku");
+        string UserInput = Console.ReadLine();
+        Console.WriteLine($"Syötit numeron {UserInput}");
+        double number = double.Parse(UserInput);
+        if (number == 0)
+            Console.WriteLine($"Numero {number} on nolla!");
+        else if (number < 0)
+            Console.WriteLine($"Numero {number} on negatiivinen!");
+        else
+            Console.WriteLine($"Numero {number} on positiivinen!");
+
+    }
+}
+}
+
+
