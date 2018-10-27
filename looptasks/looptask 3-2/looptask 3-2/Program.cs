fe421b91cc2a36b57929b65582466003e5353cc4
Marko Saarainen <marko.s.saarainen@gmail.com>
2018-10-27 11:07:30 +0300

    initialized 3-2 and done

---
+++ looptasks/looptask 3-2/looptask 3-2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace looptask_3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee annetun luvun summan");
+            int number = 0;
+            int i = 0;
+            int sum = 0;
+            do
+            {
+                Console.Write("syötä luku: ");
+                number = int.Parse(Console.ReadLine());
+
+                if (number <= 0)
+                    Console.WriteLine("Virheellinen syöte!");
+
+
+
+            } while (number <= 0);
+
+            while (i <= number)
+            {
+                sum = sum + i;
+                i = i + 1;
+            }
+            Console.WriteLine($"Luvun{number}! = {sum}");
+
+        }
+    }
+}
