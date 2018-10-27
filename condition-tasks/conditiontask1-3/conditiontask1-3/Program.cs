f66789888cbdd8be5daaf5699517f61387af60c5
Marko Saarainen <marko.s.saarainen@gmail.com>
2018-10-27 10:33:11 +0300

    condition task 1-3

---
+++ condition-tasks/conditiontask1-3/conditiontask1-3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace conditiontask1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma selvittää onko luku positiivinen vai negatiivinen sekä onko luku pariton vai parillinen.");
+            int number = 0;
+            string posOrNeg = "";
+            string evenOrOdd = "";
+
+            Console.Write("Syötä luku: ");
+            string userInput = Console.ReadLine();
+            bool ifnumber = int.TryParse(userInput, out number);
+
+            if (ifnumber)
+            {
+                if (number % 2 == 0)
+                    evenOrOdd = "parillinen";
+                else
+                    evenOrOdd = "pariton";
+
+                if (number == 0)
+                    posOrNeg = "nolla";
+                else if (number > 0)
+                    posOrNeg = "positiivinen";
+                else
+                    posOrNeg = "negatiivinen";
+            }
+            else
+                Console.WriteLine("Syöte ei ole luku.");
+
+            Console.WriteLine($"Syöttämäsi luku {number} on {posOrNeg} ja {evenOrOdd}.");
+        }
+    }
+}
