ad575dc56fcb12a6f382157553afc97c609d9e36
Marko Saarainen <marko.s.saarainen@gmail.com>
2018-10-27 11:50:37 +0300

    initialized looptask 3-3 and done

---
+++ looptasks/looptask 3-3/looptask 3-3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace looptask_3_3
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            int oddSum = 0;
+            int evenSum = 0;
+
+            Console.WriteLine("Ohjelma laskee N:n parittomien ja parillisten lukujen summan.");
+            Console.Write("Syötä luku:");
+            string userInput = Console.ReadLine();
+            bool isNumber = int.TryParse(userInput, out int number);
+
+            if (isNumber && number >= 1)
+            {
+                for (int i = 1; i <= number; i++)
+                {
+                    if (i % 2 == 0)
+                        evenSum = evenSum + i;
+                    else
+                        oddSum = oddSum + i;
+
+                }
+                Console.WriteLine($"Parittomien summa = {oddSum}, Parillisten summa = {evenSum}");
+            }
+            else
+                Console.WriteLine("Syöte ei kelpaa");
+        }
+    }
+}
+
+            
+        
+    
+    
