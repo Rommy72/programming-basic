9cbcf5014cf082a97936d4df3a0c45d63c06c9e4
Marko Saarainen <marko.s.saarainen@gmail.com>
2018-10-28 11:48:59 +0200

    looptask4-5 initialized and done

---
+++ looptasks/looptask4-5/looptask4-5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace looptask4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa vakioveikkauksen rivin  13 kohdetta, 1 = kotivoitto, X = tasapeli ja 2 = vierasvoitto)");
+
+            int i = 1;
+            double outcome = 1;
+            Random rnd = new Random();
+            do
+            {
+                outcome = rnd.NextDouble();
+
+                if (outcome <= 0.4)
+                    Console.WriteLine($"{i}. 1");
+                else if (outcome <= 0.6)
+                    Console.WriteLine($"{i}. X");
+                else
+                    Console.WriteLine($"{i}. 2");
+                i++;
+            } while (i <= 13);
+        }
+    }
+}
