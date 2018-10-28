fffd449ea6f2fe8009494e45063ddc7d6ced56c7
Marko Saarainen <marko.s.saarainen@gmail.com>
2018-10-28 11:56:39 +0200

    looptask4-6 initialized and done

---
+++ looptasks/looptask4-6/looptask4-6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace looptask4_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Ohjelma simuloi nopanheittoa 1000 kertaa ja tulostaa lisäksi kuutosen esiintymiskertojen lukumäärän.");
+
+            int i = 1;
+            int result = 1;
+            int sideSix = 0;
+            Random rnd = new Random();
+            do
+            {
+                result = rnd.Next(1, 7);
+                if (result == 6)
+                {
+                    sideSix++;
+                }
+                Console.WriteLine($"{i}. {result}");
+                i++;
+            } while (i <= 1000);
+            Console.WriteLine($"Kuutonen arvottiin {sideSix} kertaa.");
+        }
+    }
+}
