2e7a081a98cd8383773437957569bf1568b878cf
Marko Saarainen <marko.s.saarainen@gmail.com>
2018-10-28 10:25:12 +0200

    looptask4-1 done

--- looptasks/looptask4-1/looptask4.1/Program.cs
+++ looptasks/looptask4-1/looptask4.1/Program.cs
@@ -6,7 +6,16 @@ namespace looptask4._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma tulostaa kokonaisluvut 1-10 ja niiden neliöjuuret");
+            Console.WriteLine("Luku\tNeliöjuuri");
+            double square = 1;
+            int i = 1;
+
+            for (i = 1; i <= 10; i++)
+            {
+                square = Math.Sqrt(i);
+                square = Math.Round(square, 2);
+                Console.WriteLine($"{i}\t{square}");
+            }
         }
     }
-}
