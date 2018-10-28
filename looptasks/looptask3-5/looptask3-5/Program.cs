3abfc0db3e6281c7148d3d379db49bf638263c7f
Marko Saarainen <marko.s.saarainen@gmail.com>
2018-10-28 10:07:02 +0200

    looptask3-5 done

--- looptasks/looptask3-5/looptask3-5/Program.cs
+++ looptasks/looptask3-5/looptask3-5/Program.cs
@@ -6,7 +6,26 @@ namespace looptask3_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Ohjelma laskee lukua yhteen myös negatiivisilla numeroilla.");
+            Console.Write("Syötä luku: ");      
+            string userInput = Console.ReadLine();
+            bool ifNumber = int.TryParse(userInput, out int number);    
+            int sum = 0;
+
+            if (ifNumber)      
+            {
+                if (number > 0)        
+                    for (int i = 0; i <= number; i++)
+                        sum = sum + i;
+                else                    
+                    for (int i = 0; i >= number; i--)
+                        sum = sum + i;
+                Console.WriteLine($"Vastaus: {sum}");
+            }
+            else
+                Console.WriteLine("Antamasi syöte ei kelpaa");
         }
     }
 }
+
