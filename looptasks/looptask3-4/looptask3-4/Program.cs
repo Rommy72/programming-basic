b45cb149e94f33b4a716a77b716b0c484d22ea01
Marko Saarainen <marko.s.saarainen@gmail.com>
2018-10-28 09:08:01 +0200

    looptask3-4 done

--- looptasks/looptask3-4/looptask3-4/Program.cs
+++ looptasks/looptask3-4/looptask3-4/Program.cs
@@ -6,7 +6,34 @@ namespace looptask3_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number = 0;
+            int i = 1;
+            int evenSum = 0;
+            int oddSum = 0;
+
+            Console.WriteLine("Ohjema laskee parittomien ja parillisten summan");
+            
+                Console.Write("Syötä luku: ");
+            number = int.Parse(Console.ReadLine());
+            while (i <= number)
+            {
+                if (i % 2 == 0)
+                {
+                    evenSum += i; //evenSum = evenSum + i
+
+                }
+                else
+
+                {
+                    oddSum = oddSum + i;
+
+                }
+                i++;
+            }
+            Console.WriteLine($"Parillisten summa on {evenSum}");
+            Console.WriteLine($"parittomien summa on {oddSum}");
+
+
         }
     }
 }
