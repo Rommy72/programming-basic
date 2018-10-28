cecfebc123733e628399faa9906b5d6d6b9a900b
Marko Saarainen <marko.s.saarainen@gmail.com>
2018-10-28 10:34:03 +0200

    looptask4-2 initialized and done

---
+++ looptasks/looptask4-2/looptask4-2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace looptask4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ohjelma tulostaa 9 kertotaulun ");
+            Console.WriteLine("aloita ohjelman suoritus painamalla jotain nappia ");
+            Console.ReadLine();
+            for (int i = 1; i <= 9; i++)
+                for (int j = 1; j < 9; j++)
+                    Console.WriteLine($"{i}x{j}={i * j}");
+
+
+        }
+    }
+}
