19ed42d6882a420286a11f53f173dc6a70879cf7
Marko Saarainen <marko.s.saarainen@gmail.com>
2018-10-31 14:30:05 +0200

    ARRAYTASK2 initialized and done

---
+++ array/ARRAYTASK2/ARRAYTASK2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ARRAYTASK2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            double average;
+            int[] numbers = new int[100];
+            Random rnd = new Random();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(50);
+                sum += numbers[i];
+                Console.WriteLine($"{i + 1}. {numbers[i]}");
+            }
+            average = sum / numbers.Length;
+            Console.WriteLine($"\nKA. {average}");
+        }
+    }
+}
