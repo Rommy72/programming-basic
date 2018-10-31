ffe5f47cf407baeea8bee2e9961a75a05e043a15
Marko Saarainen <marko.s.saarainen@gmail.com>
2018-10-31 13:59:57 +0200

    array task 1 INITIALIZED AND DONE

---
+++ array/array task 1/array task 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace array_task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Hinnasto");
+            decimal[] productPrice = new decimal[] { 7.96m, 72.0m, 99.90m };
+            int[] productAmount = new int[] { 1, 2, 3 };
+            decimal[] productTotalprice = new decimal[3];
+            for (int i = 0; i < productTotalprice.Length;++i)
+            {
+                productTotalprice[i] = productAmount[i] * productPrice[i];
+                Console.WriteLine($"Tuote {i + 1}.{productTotalprice[i]:c}");
+            }
+        }
+    }
+}
