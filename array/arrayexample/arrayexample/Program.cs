138d9bf7d9c33ee86334f7726058b073a2f3a717
Marko Saarainen <marko.s.saarainen@gmail.com>
2018-10-31 13:04:41 +0200

    array folder init, array example init and done

---
+++ array/arrayexample/arrayexample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace arrayexample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma alustaa kokonaislukutaulukon (10 alkiota), johon arvotaan lukuja 0... 100");
+            int[] numbers = new int[10];
+            Random rnd = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(100);
+                Console.WriteLine(numbers[i]);
+
+            }
+            Console.WriteLine("Ohjelman suoritus on päättynyt");
+
+            
+
+            
+
+
+        }
+    }
+}
