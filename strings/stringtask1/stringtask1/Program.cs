9f90636783bbcc45823dacbea406d927351213d6
Marko Saarainen <marko.s.saarainen@gmail.com>
2018-11-12 09:07:49 +0200

    string 1 initialized and done

---
+++ strings/stringtask1/stringtask1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace stringtask1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee käyttäjän syötteestä merkkien määrän.");
+            string strCount = UserInput();
+
+            Console.WriteLine($"Kirjoitit {strCount}.\n Siinä on {LengthOfString(ref strCount)} merkkiä.");
+
+        }
+        static string UserInput()
+        {
+            Console.Write("Syötä lause: ");
+            string userInput = Console.ReadLine();
+            return userInput;
+        }
+
+        static int LengthOfString(ref string a)
+        {
+            int length = a.Length;
+            return length;
+        }
+    }
+}
