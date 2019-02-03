de1c1f538b7b71b2b0dedaff838eda7726d80b48
Marko Saarainen <marko.s.saarainen@gmail.com>
2019-02-03 12:01:50 +0200

    selfvalue 1 initialized and done

---
+++ selfvalue/selfvalue 1/selfvalue 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace selfvalue_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+                 Console.WriteLine("Syötä sana tai lause");
+            string userInput = Console.ReadLine();
+
+            for (int i = 0; i< 5; i++)
+            {
+                Console.WriteLine($"{i + 1}. {userInput}");
+            }
+    Console.ReadKey();
+        }
+    }
+}
